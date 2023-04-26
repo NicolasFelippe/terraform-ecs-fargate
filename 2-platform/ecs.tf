851038a19540570905cc92987f1a1dfc4e23ddea
Nicolas Felippe Rocha <nicolas.rocha@agapys.com>
2023-04-26 10:41:27 -0300

    init project

---
+++ 2-platform/ecs.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = var.region
+}
+
+terraform {
+  backend "s3" {}
+}
+
+data "terraform_remote_state" "infrastructure" {
+  backend = "s3"
+
+  config = {
+    region = var.region
+    bucket = var.remote_state_bucket
+    key    = var.remote_state_key
+  }
+}
+
+resource "aws_ecs_cluster" "production-fargate-cluster" {
+  name = "Production-Fargate-Cluster"
+}
+
+resource "aws_alb" "ecs_cluster_alb" {
+  name     = "${var.ecs_cluster_name}-ALB"
+  internal = false
+  security_groups {
+
+  }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+terraform init -backend-config="infrastructure-prod.config"
+terraform plan -var-file="production.tfvars"
+terraform apply -var-file="production.tfvars"
